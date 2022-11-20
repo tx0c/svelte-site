67280200310e7d442dcadb5fd5f162d00f40a243
Aka jazzy <mingchenliang954@gmail.com>
2022-11-20 23:44:45 +0800

    fix(404): add styles to 404 page

--- routes/__error.svelte
+++ routes/__error.svelte
@@ -1,23 +1,57 @@
+
 <script lang="ts">
   import { onMount } from "svelte";
-  import { goto } from "$app/navigation";
-
+  // import { goto } from "$app/navigation";
   onMount(() => {
-    const timer = setTimeout(() => goto("/", { replaceState: true }), 0e3);
-    return () => clearTimeout(timer);
+    // const timer = setTimeout(() => goto("/", { replaceState: true }), 0e3);
+    // return () => clearTimeout(timer);
   });
 </script>
 
-<p>page not found, <a href="/">return to home page</a></p>
+<section class="error-page">
+  <img src="img/404page.svg" alt="404Image" >
+  <p>Page not found.
+ </p>
+  <p>
+     Redirecting you to home page...
+  </p>
+  <a class="btn outline" href="/"><span>BACK TO HOME</span></a>
+
+</section>
 
 <style>
-  p::before {
+  .error-page::before {
     display: block;
     content: "";
-    padding-top: 6rem;
   }
-
-  p {
+ 
+  .error-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  @media(min-width: 768px) {
+      .error-page img {
+    width: 900px;
+    height: 628px;
+    margin-bottom: 8px;
+  }
+  .error-page p{
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 48px;
     text-align: center;
   }
+
+  .error-page a {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    padding: 12px 16px;
+    margin-top: 40px;
+    width: 397px;
+  }
+  }
+
 </style>
