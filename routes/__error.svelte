120d5badc6c188cd81a2bf83f7c932a103be3096
Aka jazzy <mingchenliang954@gmail.com>
2022-11-21 11:37:01 +0800

    fix(404): add mobile styles

--- routes/__error.svelte
+++ routes/__error.svelte
@@ -1,4 +1,3 @@
-
 <script lang="ts">
   import { onMount } from "svelte";
   // import { goto } from "$app/navigation";
@@ -9,49 +8,57 @@
 </script>
 
 <section class="error-page">
-  <img src="img/404page.svg" alt="404Image" >
-  <p>Page not found.
- </p>
-  <p>
-     Redirecting you to home page...
-  </p>
+  <img src="img/404page.svg" alt="404Image" />
+  <p>Page not found.</p>
+  <p>Redirecting you to home page...</p>
   <a class="btn outline" href="/"><span>BACK TO HOME</span></a>
-
 </section>
 
 <style>
-  .error-page::before {
-    display: block;
-    content: "";
-  }
- 
   .error-page {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
-  @media(min-width: 768px) {
-      .error-page img {
-    width: 900px;
-    height: 628px;
-    margin-bottom: 8px;
+
+  .error-page img {
+    width: 100%;
+    height: 261px;
+    margin-top: 88px;
   }
-  .error-page p{
-    font-size: 36px;
+
+  .error-page p {
+    font-size: 14px;
     font-weight: 700;
-    line-height: 48px;
-    text-align: center;
   }
 
   .error-page a {
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 24px;
-    padding: 12px 16px;
-    margin-top: 40px;
-    width: 397px;
-  }
-  }
+      width: 343px;
+      padding: 12px 16px;
+    }
 
+  @media (min-width: 768px) {
+    .error-page img {
+      margin: 0 510px;
+      height: 375px;
+      margin-top: 68px;
+      margin-bottom: 8px;
+    }
+    .error-page p {
+      font-size: 36px;
+      font-weight: 700;
+      line-height: 48px;
+      text-align: center;
+    }
+
+    .error-page a {
+      font-size: 18px;
+      font-weight: 400;
+      line-height: 24px;
+      padding: 12px 16px;
+      margin-top: 40px;
+      width: 397px;
+    }
+  }
 </style>
